8d4add46d0ce2fa2e124981599176fff0a399424
ReaperClown <the.willian.mesquita@gmail.com>
2021-07-13 21:16:07 -0300

    New exercices added

---
+++ santander-bootcamp-fullstack-developer/projetos/desenvolvimento-basico-java/example-maven/src/test/java/com/project/examplemaven/ExampleMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.examplemaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
